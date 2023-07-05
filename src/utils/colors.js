6e84eb04f58975ff38d92b9a13de7b7c12d4748c
Hech-z19 <78944717+Hech-z19@users.noreply.github.com>
2023-07-04 23:37:46 -0400

    Utilidad de colores

---
+++ src/utils/colors.js
@@ -0,0 +1,12 @@
+const colors = {
+  primary: '#4E4FEB',
+  secondary: '#068FFF',
+  success: '#28a745',
+  danger: '#dc3545',
+  warning: '#ffc107',
+  info: '#17a2b8',
+  light: '#EEEEEE',
+  dark: '#000000',
+};
+
+export default colors;
